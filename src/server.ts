46b89bd3e9779e0a445e6892f2fcbfaf54ee79ef
Bakare Abiola <iamstarcode@gmail.com>
2024-04-16 18:12:13 +0100

    chore: update

--- src/server.ts
+++ src/server.ts
@@ -12,9 +12,7 @@ app.get('/', (req: Request, res: Response) => {
 });
 
 app.get('/search', async (req: Request, res: Response) => {
-  console.log(req.query.query, process.env.TMDB_API_READ_ACCESS_TOKEN);
   try {
-    // Construct the URL with query parameters
     apiUrl.searchParams.append('query', req.query.query as string);
     apiUrl.searchParams.append(
       'include_adult',
@@ -26,26 +24,21 @@ app.get('/search', async (req: Request, res: Response) => {
     );
     apiUrl.searchParams.append('page', (req.query.page as string) || '1');
 
-    // Make a GET request to the TMDB API using fetch and include authorization header
     const response = await fetch(apiUrl.toString(), {
       headers: {
         Authorization: `Bearer ${process.env.TMDB_API_READ_KEY}`, // Replace with your access token
       },
     });
 
-    // Check if the response was successful
     if (!response.ok) {
       throw new Error('Failed to fetch data from TMDB API');
     }
 
-    // Parse the response JSON
     const data = await response.json();
 
-    // Return the response data from the TMDB API
     res.json(data);
   } catch (error) {
     console.log(error);
-    // If there's an error, return an error response
     res.status(500).json({ error: 'Internal Server Error' });
   }
 });
